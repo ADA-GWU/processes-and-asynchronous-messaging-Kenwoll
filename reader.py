2b2babb9a43f09cf9626daccda903b15d2287ea5
Kenwoll <i.murad.2003@gmail.com>
2023-10-28 18:35:50 +0400

    code update

--- reader.py
+++ reader.py
@@ -1,16 +1,19 @@
-"""Python module for enabling threading"""
 import threading
 import time
 import json
 import psycopg2
 
+SELECT_QUERY = "SELECT SENDER_NAME, MESSAGE, SENT_TIME FROM ASYNC_MESSAGES WHERE RECEIVED_TIME " \
+                "IS NULL AND SENDER_NAME != %s FOR UPDATE SKIP LOCKED"
+
+UPDATE_QUERY = "UPDATE ASYNC_MESSAGES SET RECEIVED_TIME = timestamp 'epoch'" \
+                "+ %s * interval '1 second' WHERE SENDER_NAME = %s " \
+                "AND MESSAGE = %s AND SENT_TIME = %s"
+
 def read_db_config(filename):
     """Function to read database configurations from a JSON file"""
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
-
-# Read database configurations from the JSON file
-db_configs = read_db_config('db_config.json')
 
 def connect_to_databases(configs):
     """Function to create connections to multiple databases"""
@@ -23,27 +26,20 @@ def connect_to_databases(configs):
             host=config["host"],
             port=config["port"]
         )
+
         connections.append(connection)
+
     return connections
 
-# Connect to multiple databases
-connections = connect_to_databases(db_configs)
-
-def fetch_and_update_message(your_name, connection):
+def fetch_and_update_message(reader_name, connection):
     """Function fetching an dupdating databse rows"""
     cursor = connection.cursor()
     try:
-        cursor.execute(
-            "SELECT SENDER_NAME, MESSAGE, SENT_TIME FROM ASYNC_MESSAGES WHERE RECEIVED_TIME " \
-                "IS NULL AND SENDER_NAME != %s FOR UPDATE SKIP LOCKED",
-            (your_name,))
+        cursor.execute(SELECT_QUERY, (reader_name,))
         row = cursor.fetchone()
         if row:
             sender_name, message_text, message_time = row
-            cursor.execute("UPDATE ASYNC_MESSAGES SET RECEIVED_TIME = timestamp 'epoch'" \
-                                "+ %s * interval '1 second' WHERE SENDER_NAME = %s " \
-                                "AND MESSAGE = %s AND SENT_TIME = %s",
-                           (time.time(), sender_name, message_text, message_time))
+            cursor.execute(UPDATE_QUERY, (time.time(), sender_name, message_text, message_time))
             connection.commit()
             return sender_name, message_text, message_time
         else:
@@ -66,6 +62,14 @@ class ReaderThread(threading.Thread):
             if message:
                 sender_name, message_text, message_time = message
                 print(f"Sender {sender_name} sent {message_text} at time {message_time}.")
+
+
+
+# Read database configurations from the JSON file
+db_configs = read_db_config('db_config.json')
+
+# Connect to multiple databases
+connections = connect_to_databases(db_configs)
 
 # Create and start the reader thread
 reader_threads = []
--- sender.py
+++ sender.py
@@ -1,14 +1,18 @@
-"""Python module for enabling threading"""
 import json
+import time
+import random
+import threading
 import psycopg2
+
+SENDER_NAME = "Murad"
+
+INSERT_QUERY = "INSERT INTO ASYNC_MESSAGES(SENDER_NAME, MESSAGE, SENT_TIME) " \
+                "VALUES(%s, %s,  to_timestamp(%s))"
 
 def read_db_config(filename):
     """Function to read database configurations from a JSON file"""
     with open(filename, 'r', encoding='utf-8') as file:
         return json.load(file)
-
-# Read database configurations from the JSON file
-db_configs = read_db_config('db_config.json')
 
 def connect_to_databases(configs):
     """Function to create connections to multiple databases"""
@@ -21,8 +25,50 @@ def connect_to_databases(configs):
             host=config["host"],
             port=config["port"]
         )
+
         connections.append(connection)
+    
     return connections
+
+def insert_message(connection, message):
+    """Function to insert a record into the table"""
+    cursor = connection.cursor()
+    try:
+        cursor.execute(INSERT_QUERY, (SENDER_NAME, message, time.time()))
+        connection.commit()
+    finally:
+        cursor.close()
+
+# Read database configurations from the JSON file
+db_configs = read_db_config('db_config.json')
 
 # Connect to multiple databases
 connections = connect_to_databases(db_configs)
+
+def choose_thread_and_insert_message(threads, message):
+    chosen_thread = random.choice(threads)
+    chosen_connection = chosen_thread.connection
+    print(f"{chosen_thread.name} sends message")
+    insert_message(chosen_connection, message)
+
+class SenderThread(threading.Thread):
+    def __init__(self, name, connection):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.connection = connection
+
+    def run(self):
+        while True:
+            user_input = input("Enter your message: ")
+            choose_thread_and_insert_message(sender_threads, user_input)
+
+sender_threads = []
+for i, connection in enumerate(connections):
+    thread_name = f"Sender_{i+1}"
+    sender_thread = SenderThread(thread_name, connection)
+    sender_threads.append(sender_thread)
+    sender_thread.start()
+
+# Main thread waits for the sender threads to complete
+for thread in sender_threads:
+    thread.join()
